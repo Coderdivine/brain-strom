365bfb77968f2ec1741d0571ced48d1d60b6d986
Coderdivine <chimdi4332@gmail.com>
2022-07-25 22:27:03 +0100

    makes done

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const axios = require("axios");
 const Web3 = require("web3");
-const coingecko = require("coingecko-api");
+const web3 = new Web3("https://bsc-dataseed.binance.org/");
+const Coingecko = require("coingecko-api");
 require("dotenv").config();
-const address = "";
+const Models = require("./model.js");
+const address = "0x82f1A8F5460B24810C52BDB4416425eCB60C048A";
 const privateKey = process.env.PRIVATEKEY;
 const BscContracts = require("./Bot.json");
+const CoinGeckoClient = new Coingecko();
 //watch both bnb and btc on bsc bep20...
+async function getCoinId(){
+    let a_ = await CoinGeckoClient.coins.list();
+    return a_;
+}
+async function getCurrentPrice(){
+    let data = await CoinGeckoClient.coins.fetch('bitcoin', {});
+    return data;
+};
+// /getCoinId().then(console.log)
+getCurrentPrice().then(console.log)
